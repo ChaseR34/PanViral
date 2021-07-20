d54d6293f22c9ea14912eb48f25bc3b0283487df
ChaseR34 <33583805+ChaseR34@users.noreply.github.com>
2021-07-20 11:38:41 -0700

    adding q2-pan-classifier directory for qiime2 plugin

---
+++ q2-pan-classifier/q2_pan_classifier/plugin_setup.py
@@ -0,0 +1,44 @@
+#   Copyright 2021 Evan Bolyen
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import importlib
+
+from qiime2.plugin import Str
+from qiime2.plugin import Plugin, SemanticType, model
+
+import q2_pan_classifier.actions as actions
+
+from q2_pan_classifier.format_types import MyString
+
+from q2_types.feature_table import FeatureTable, Frequency
+# This is the plugin object. It is what the framework will load and what an
+# interface will interact with. Basically every registration we perform will
+# involve this object in some way.
+plugin = Plugin("pan_classifier", version="0.0.1.dev",
+                website="https://github.com/ebolyen/q2-reveal")
+
+plugin.register_semantic_types(MyString)
+
+
+plugin.methods.register_function(
+    function=actions.create_classifier.create_classifier,
+    inputs=None,
+    outputs=[('cool', MyString)],
+    parameters={},
+    input_descriptions=None,
+    parameter_descriptions=None,
+    name='Pan Classifier',
+    description="Creates a classifier and classifies them using machine learning"
+)
+
+# importlib.import_module("q2_reveal.transformers")
